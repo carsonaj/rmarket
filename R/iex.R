54b832a973facfa583def4b15e280accf7b855a8
Carson <carsonaj@tutanota.com>
2019-12-07 14:07:42 -0600

    commit

--- R/iex.R
+++ R/iex.R
@@ -1,17 +1,17 @@
-library("httr")
-library("jsonlite")
-
+#' @export
 get.price.data <- function(ticker, length=200) {
-    api_key <- "pk_06ea5c94527b4754a9196efb576df932"
+    api_key <- "pk_bad2e64778134e27a0e02addcaed2405"
     url <- paste(
         "https://cloud.iexapis.com/stable/stock/",
         ticker, "/chart/max?token=",
         api_key, sep=""
     )
     query <- list(chartLast=length)
-    request <- GET(url, query=query)
-    response <- content(request, as="text")
-    price.df <- fromJSON(response, flatten = TRUE)
+    request <- httr::GET(url, query=query)
+    response <- httr::content(request, as="text")
+    price.df <- jsonlite::fromJSON(response, flatten = TRUE)
     price.data <- df[c("date", "close")]
+
+    return(price.data)
 
 }
