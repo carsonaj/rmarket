03e36c74dd8324601b79804a0c0ac0420633bb7a
Carson <carsonaj@tutanota.com>
2019-12-08 08:55:36 -0600

    commit

--- R/technical.R
+++ R/technical.R
@@ -1,17 +1,18 @@
-sma <- function(price.df, length=20) {
+moving.average <- function(price.df, length=20) {
     avg <- as.numeric(
         stats::filter(price.df$close, rep(1/length, length), sides = 1)
     )
-    avg.df <- data.frame(matrix(ncol=2, nrow=length(price.df$date)))
-    colnames(avg.df) <- c("date", "moving.average")
+    avg.df <- data.frame(matrix(ncol=3, nrow=length(price.df$date)))
+    colnames(avg.df) <- c("date", "close", "moving.average")
     avg.df$date <- price.df$date
+    avg.df$close <- price.df$close
     avg.df$moving.average <- avg
 
     return(avg.df)
 }
 
 bollinger.bands <- function(price.df, length=20) {
-    bbands.df <- sma(price.df, length)
+    bbands.df <- movin.average(price.df, length)
     dev <- as.numeric(
         stats::filter(
             (price.df$close - bbands.df$moving.average)^2, rep(1/(length-1), length), sides = 1
@@ -25,4 +26,13 @@ bollinger.bands <- function(price.df, length=20) {
     bbands.df["upper.band"] <- upper.band
 
     return(bbands.df)
+}
+
+bollinger.bands.plot <- function(bbands.df) {
+    ggplot() +
+    geom_line(data=bbands.df[c("date", "close")], aes(x=date, y=close), color="black") +
+    geom_line(data=bbands.df[c("date", "moving.average")], aes(x=date, y=moving.average), color="red") +
+    geom_line(data=bbands.df[c("date", "upper.band")], aes(x=date, y=upper.band), color="red", linetype="dashed") +
+    geom_line(data=bbands.df[c("date", "lower.band")], aes(x=date, y=lower.band), color="red", linetype="dashed")+
+    xlab("date") + ylab("price") 
 }
